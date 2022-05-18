821e60229456c2f195efab17973df944ae2c08b2
Zarema Tassyrova <zarema.tsr@gmail.com>
2022-05-18 09:58:15 -0400

    backend components

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# book-react-app-backend
+# book-react-app
+
+## Project Description
+I want to create an app that tracks what books you read, allows you to browse new books and have a zoom meeting for book club discussions.
+
+## Link to the API you plan to use
+https://developers.google.com/books/docs/v1/getting_started
+
+## Wireframes
+
+## Component Hieratchy
+
+## User Stories
+As a user, I want to be able to add newly read books and leave my description of it. Also, I want to let other users leave their review/comments.
+
+As a user, I want  to be able to add or delete books that I read.
+
+As a user, I want to have book club meetings to discuss a particular book.
+
+## MVP Goals
+Display 4 separate pages: Main/Home page, BookWall page(with already read books), BookShop page(search for future reads), BookClub page.
+Build Database with full functioning CRUD
+Create route for adding new books
+Delete route for deleting books
+Search route for looking for new books
+Show Route to show details
+Heroku and Netlify Deployment
+
+
+
+## Stretch Goal
+Create user auth
+Search for books on BookShop Page
+Full CRUD
---
+++ server.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const {PORT = 3000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+//import middleware
+const cors = require ('cors');
+const morgan = require ('morgan');
+
+//database connection
+mongoose.connect(MONGODB_URL);
+
+//connecting events
+mongoose.connection
+.on("open", () => console.log("Your are connected to mongoose"))
+.on("close", () => console.log("Your are disconnected from mongoose"))
+.on("error", (error) => console.log(error));
+
+//models
+const BooksSchema = new mongoose.Schema({
+    title: String,
+author: String,
+category: String,
+image: String,
+review: String
+})
+
+const Books = mongoose.model('Books', BooksSchema);
+
+//middleware
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+//ROUTES
+app.get("/", (req,res) => {
+    res.send ("hello world")
+});
+
+app.get('/books', async (req,res) => {
+    try {
+        res.json(await Books.find({}));
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post('/books', async (req, res) => {
+    try {
+        res.json(await Books.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)  
+    }
+})
+
+app.put('/books/:id', async (req,res) => {
+    try {
+       res.json(await Books.findByIdAndUpdate(req.params.id, req.body)) 
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.delete('/books/:id', async (req, res) => {
+    try {
+        res.json(await Books.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
